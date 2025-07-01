931118621866790916039d1bb92ff416265615ac
AravindMandan <aravind.mandan@minfytech.com>
2025-07-01 18:31:47 +0530

    this ml flow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import data_ingest
 import preprocessing
 from sklearn.model_selection import train_test_split
-import model_selection
+import model_selection_mlflow
 import train_save_model
 df=data_ingest.data_load('Bank_Personal_Loan_Modelling.xlsx','Data')
 
@@ -13,10 +13,10 @@ y = df['Personal Loan']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
  
 # Run model evaluation
-model_selection.evaluate_models_with_grid_search(X_train, X_test, y_train, y_test)
+#model_selection.evaluate_models_with_grid_search(X_train, X_test, y_train, y_test)
 #  Find best model
 
-best_model, best_params = model_selection.evaluate_models_with_grid_search(X_train, X_test, y_train, y_test)
+best_model, best_params = model_selection_mlflow.evaluate_models_with_grid_search(X_train, X_test, y_train, y_test)
  #Train on full data and save
 
 train_save_model.train_and_save_final_model(X, y, best_model, model_name="best_final_model.pkl")
